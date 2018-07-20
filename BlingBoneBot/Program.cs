f1b6654a5bd04f26fd23901af21700bdbe38621a
Jesse Robinson <jrobinson@bhg-inc.com>
2018-07-20 15:28:51 -0400

    Initial Commit

---
+++ BlingBoneBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace BlingBoneBot
+{
+    class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("687326924:AAHizZUCtchrimaqyjCew5CU2UmS1A-NmXw");
+
+        static void Main(string[] args)
+        {
+
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                if (e.Message.Text == "How are you?");
+            Bot.SendTextMessageAsync(e.Message.Chat.Id, "Fuck off bitch ヾ(●ω●)ノ ");
+        }
+
+        
+
+    }
+}
