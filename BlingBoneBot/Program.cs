91747aad5b18efb13c7a29aafd1e511a5b0b8737
Jesse Robinson <jrobinson@bhg-inc.com>
2018-07-20 17:22:38 -0400

    Adding stickers

--- BlingBoneBot/Program.cs
+++ BlingBoneBot/Program.cs
@@ -10,6 +10,7 @@ namespace BlingBoneBot
     class Program
     {
         private static readonly TelegramBotClient Bot = new TelegramBotClient("687326924:AAHizZUCtchrimaqyjCew5CU2UmS1A-NmXw");
+        private List<String> 
 
         static void Main(string[] args)
         {
@@ -23,11 +24,28 @@ namespace BlingBoneBot
         private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
-                if (e.Message.Text == "Hey");
-            Bot.SendTextMessageAsync(e.Message.Chat.Id, "Fuck off bitch ヾ(●ω●)ノ ");
+            {
+                String userText = e.Message.Text.ToString().ToLower();
+                if (userText.Contains("hey") == true)
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "What the fuck do yo' want? ヾ(●ω●)ノ ");
+                Task.Delay(3000);
+                    Bot.SendStickerAsync(e.Message.Chat.Id, "CAADAgADggAD0lqIAcrj5QvkZIUPAg"); // Gun Pepe
+                
+                //CAADAgADSioAAktqAwABYJfJOAN1P1MC
+
+            }
+
+            //if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Sticker)
+            //{
+            //    Bot.GetChatAsync(e.Message.Chat.Id);
+            //    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Sticker Received");
+
+            //}
+
+          //  Bot.SendStickerAsync(e.Message.Chat.Id, "CAADAgADSioAAktqAwABYJfJOAN1P1MC"); Nasty Pepe
         }
 
-        
+
 
     }
 }
