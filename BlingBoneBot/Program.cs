cceb7828c125b8cac22bae736093dd836e12e8c4
Jesse Robinson <Jesse.michael.robinson@gmail.com>
2018-08-26 11:33:40 -0400

    Initial Commit

--- BlingBoneBot/Program.cs
+++ BlingBoneBot/Program.cs
@@ -2,50 +2,119 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Args;
+using System.Diagnostics;
+using System.Timers;
+
 
 namespace BlingBoneBot
 {
     class Program
     {
-        private static readonly TelegramBotClient Bot = new TelegramBotClient("687326924:AAHizZUCtchrimaqyjCew5CU2UmS1A-NmXw");
-        private List<String> 
+        private static string accessToken = "";
+        private static readonly TelegramBotClient botClient = new TelegramBotClient(accessToken);
+        
+        private static string blingyDingyGroupID = "";
+        private static int bedTimeHour = 23;
+
+        // Random Frog List
+        static List<string> uglyFrogStickers = new List<string>();
+        static Random rnd = new Random();
+        static int randomFrog = rnd.Next(uglyFrogStickers.Count);
 
         static void Main(string[] args)
         {
+            // Getting all ugly frogs and putting them into a list to randomly select
+            uglyFrogStickers.Add("CAADBAADZj0AAnrRWwamey84lXdwzwI");
+            uglyFrogStickers.Add("CAADBAADEQ4AAnrRWwZZyqx4aM84TwI");
+            uglyFrogStickers.Add("CAADBAAD9Q0AAnrRWwaim8djGnYxPAI");
+            uglyFrogStickers.Add("CAADBAAD6hMAAnrRWwbaJnXW0KcRqQI");
+            uglyFrogStickers.Add("CAADBAADEhQAAnrRWwbRfw3ebmOXRgI");
+            uglyFrogStickers.Add("CAADBAADCw4AAnrRWwZKTo8Om5qu5AI");
+            uglyFrogStickers.Add("CAADBAADgw4AAnrRWwaCxy8EtZSqTwI");
+            uglyFrogStickers.Add("CAADBAADXAUAAlMDUREwv143fGWkmQI");
 
-            Bot.OnMessage += Bot_OnMessage;
-            Bot.StartReceiving();
-            Console.ReadLine();
-            Bot.StopReceiving();
+
+            BedTimeSchedule();
+            botClient.OnMessage += Bot_OnMessage;
+            botClient.StartReceiving();
+
+            Thread.Sleep(999000);
+
+
         }
 
-        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        private static void BedTimeSchedule()
         {
+
+            var runAt = DateTime.Today + TimeSpan.FromHours(bedTimeHour);
+
+            if (runAt <= DateTime.Now)
+            {
+                SendGoodNight();
+            }
+            else
+            {
+                var delay = runAt - DateTime.Now;
+                System.Threading.Tasks.Task.Delay(delay).ContinueWith(_ => SendGoodNight());
+            }
+
+        }
+
+        private static void SendGoodNight()
+        {
+            botClient.SendTextMessageAsync(
+            chatId: blingyDingyGroupID,
+            text: "GO TO SLEEP!!~~ (づ｡◕‿‿◕｡)づ ");
+
+            botClient.SendAnimationAsync(
+                chatId: blingyDingyGroupID,
+                animation: "https://i.imgur.com/q3e87zR.gif"
+                ); // Cat & Shaq GIF
+
+            botClient.SendStickerAsync(
+            chatId: blingyDingyGroupID,
+            sticker: "CAADBAADYggAAsHKsAO-vakd5YxL3wI"); // Snoopy Sleeping
+
+        }
+
+
+
+        static async void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
                 String userText = e.Message.Text.ToString().ToLower();
-                if (userText.Contains("hey") == true)
-                    Bot.SendTextMessageAsync(e.Message.Chat.Id, "What the fuck do yo' want? ヾ(●ω●)ノ ");
-                Task.Delay(3000);
-                    Bot.SendStickerAsync(e.Message.Chat.Id, "CAADAgADggAD0lqIAcrj5QvkZIUPAg"); // Gun Pepe
-                
-                //CAADAgADSioAAktqAwABYJfJOAN1P1MC
-
+                if (userText.Contains("ugly frog") == true)
+                {
+                    await botClient.SendStickerAsync(
+                      chatId: e.Message.Chat,
+                      sticker: uglyFrogStickers[randomFrog]); // Random frog
+                }
             }
 
-            //if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Sticker)
-            //{
-            //    Bot.GetChatAsync(e.Message.Chat.Id);
-            //    Bot.SendTextMessageAsync(e.Message.Chat.Id, "Sticker Received");
-
-            //}
-
-          //  Bot.SendStickerAsync(e.Message.Chat.Id, "CAADAgADSioAAktqAwABYJfJOAN1P1MC"); Nasty Pepe
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                String userText = e.Message.Text.ToString().ToLower();
+                if (userText.Contains("good night") == true)
+                {
+                    await botClient.SendStickerAsync(
+                      chatId: e.Message.Chat,
+                      sticker: "CAADBAADYggAAsHKsAO-vakd5YxL3wI"); // Snoopy Sleeping
+                }
+            }
         }
-
-
-
     }
 }
+
+    
+
+
+
+
+
+
